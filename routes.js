2ebe88665bde80dee01f4264158e7b8879922dcd
Terrence Tse <ttse@salesforce.com>
2017-04-17 13:22:56 -0700

    URL Change

--- routes.js
+++ routes.js
@@ -12,21 +12,20 @@ module.exports = function(app) {
 		res.redirect(oauth2.getAuthorizationUrl());
 	});
 
-	app.get('/auth/callback', function(req, res) {
-		var conn = new jsforce.Connection({oauth2: oauth2});
-		var code = req.param('code');
-		conn.authorize(code, function(err, userInfo) {
-			if (err) {
-				return console.error(err);
-			}
-			req.session.accessToken = conn.accessToken;
-			req.session.instanceUrl = conn.instanceUrl;
-			req.session.refreshToken = conn.refreshToken;
-			console.log(conn.refreshToken);
-			
-			URL = "URL which I'm using for oauth"
-			res.redirect('/dfty/case');
-		});
+	app.get('/oauth2/callback', function(req, res) {
+	  var conn = new sf.Connection({ oauth2 : oauth2 });
+	  var code = req.param('code');
+	  conn.authorize(code, function(err, userInfo) {
+	    if (err) { return console.error(err); }
+	    // Now you can get the access token, refresh token, and instance URL information.
+	    // Save them to establish connection next time.
+	    console.log(conn.accessToken);
+	    console.log(conn.refreshToken);
+	    console.log(conn.instanceUrl);
+	    console.log("User ID: " + userInfo.id);
+	    console.log("Org ID: " + userInfo.organizationId);
+	    // ...
+	  });
 	});
 
 	app.get("/", function(req, res) {
