c3feae98ba3febc7f31f0f0b89829e767557fdb7
Terrence Tse <ttse@salesforce.com>
2017-04-17 14:31:25 -0700

    URL Change

--- routes.js
+++ routes.js
@@ -5,14 +5,14 @@ module.exports = function(app) {
 	var oauth2 = new jsforce.OAuth2({
 		clientId: process.env.CLIENT_ID,
 		clientSecret: process.env.CLIENT_SECRET,
-		redirectUri: process.env.BASE_URL + '/oauth2/callback'
+		redirectUri: process.env.BASE_URL
 	});
 
 	app.get('/oauth2/auth', function(req, res) {
 		res.redirect(oauth2.getAuthorizationUrl());
 	});
 
-	app.get('/oauth2/callback', function(req, res) {
+	app.get('/', function(req, res) {
 	  var conn = new sf.Connection({ oauth2 : oauth2 });
 	  var code = req.param('code');
 	  conn.authorize(code, function(err, userInfo) {
@@ -28,30 +28,30 @@ module.exports = function(app) {
 	  });
 	});
 
-	app.get("/", function(req, res) {
+	// app.get("/", function(req, res) {
 
-		// Put this into a Utils module...
-		var conn = new jsforce.Connection({
-			oauth2: {
-				loginUrl: "https://login.salesforce.com",
-				clientId: process.env.CLIENT_ID,
-				clientSecret: process.env.CLIENT_SECRET,
-				redirectUri: process.env.BASE_URL
-			},
-			instanceUrl: process.env.INSTANCE_URL,
-			refreshToken: process.env.REFRESH_TOKEN
-		});
+	// 	// Put this into a Utils module...
+	// 	var conn = new jsforce.Connection({
+	// 		oauth2: {
+	// 			loginUrl: "https://login.salesforce.com",
+	// 			clientId: process.env.CLIENT_ID,
+	// 			clientSecret: process.env.CLIENT_SECRET,
+	// 			redirectUri: process.env.BASE_URL
+	// 		},
+	// 		instanceUrl: process.env.INSTANCE_URL,
+	// 		refreshToken: process.env.REFRESH_TOKEN
+	// 	});
 
-		conn.on("refresh", function(accessToken, connRes) {
-			console.log("Access Token: " + accessToken);
-			res.redirect("lightningOutTest.html#accessToken=" + accessToken);
-		});
+	// 	conn.on("refresh", function(accessToken, connRes) {
+	// 		console.log("Access Token: " + accessToken);
+	// 		res.redirect("lightningOutTest.html#accessToken=" + accessToken);
+	// 	});
 
-		conn.identity( function(connErr, idenRes) {
-			if(connErr) {
-				res.status(400).send(connErr);
-			}
-		});
+	// 	conn.identity( function(connErr, idenRes) {
+	// 		if(connErr) {
+	// 			res.status(400).send(connErr);
+	// 		}
+	// 	});
 		
-	});
+	// });
 };
