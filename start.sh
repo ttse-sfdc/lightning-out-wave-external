69aae548001ea9d8ed31c9b223ed3e3ba0c44072
Bing <by@aplusd.co>
2017-06-17 21:50:18 -0700

    Added code for PRD testing

--- start.sh
+++ start.sh
@@ -1,7 +1,16 @@
 export BASE_URL=http://localhost:3123 && \
 export LOGIN_URL=https://login.salesforce.com && \
+
+# DEV
 export CLIENT_ID=3MVG9SemV5D80oBe9YTRxk_b.CeEzBMGBM4sQJUW8MBZjtESSLKCoLNSYn_d79r_qw.3NOExoVZ4gQxL6RCeZ && \
 export CLIENT_SECRET=5930902767596891690 && \
 export INSTANCE_URL=https://externalwavetest.my.salesforce.com && \
 export REFRESH_TOKEN=5Aep8614XGwzVzzTO54CmDqQVeOWipsp2bIlQ2AJEdhlFttfrBcyU2JFJP9wpp0604TA2exvN3Uzxg4tseTdtFm && \
+
+# # PRD
+# export CLIENT_ID=3MVG9CEn_O3jvv0xjKzbd8anvY_irzh5P.fA7JfAcNp7iTtDUIbDguPQJcTH7vyZZRmpdCfBlu3F5ppW_bthI && \
+# export CLIENT_SECRET=7025190072216292682 && \
+# export INSTANCE_URL=https://ac-playground.my.salesforce.com && \
+# export REFRESH_TOKEN=5Aep861w1PjQhFtnoCiNFg6uY3P30luA73HJ_HVLtvLlkzk.6GXa6x2z3BUI7xglzcTVXMOp0hgiFHNcar5scZ. && \
+
 node app.js
