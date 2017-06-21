f30669aaae20f561ff06b8bc9b0505a64fd7b267
Terrence Tse <ttse@salesforce.com>
2017-06-21 11:50:32 -0700

    Updated refresh token

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ export LOGIN_URL=https://login.salesforce.com && \
 export CLIENT_ID=3MVG9SemV5D80oBe9YTRxk_b.CeEzBMGBM4sQJUW8MBZjtESSLKCoLNSYn_d79r_qw.3NOExoVZ4gQxL6RCeZ && \
 export CLIENT_SECRET=5930902767596891690 && \
 export INSTANCE_URL=https://externalwavetest.my.salesforce.com && \
-export REFRESH_TOKEN=5Aep8614XGwzVzzTO54CmDqQVeOWipsp2bIlQ2AJEdhlFttfrBcyU2JFJP9wpp0604TA2exvN3Uzxg4tseTdtFm && \
+export REFRESH_TOKEN=5Aep8614XGwzVzzTO54CmDqQVeOWipsp2bIlQ2Akykuk_VxlLXxvi3yjcd_mt1AA_5yXEte74NZ4_wmSQWZtq_V && \
 node app.js
