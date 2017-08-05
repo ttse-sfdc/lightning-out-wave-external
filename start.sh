7b0e473bf35500899ecb6c2fa1a425ffac17d1ad
Terrence Tse <ttse@salesforce.com>
2017-08-04 22:06:13 -0700

    Service Org Test

--- start.sh
+++ start.sh
@@ -1,12 +1,17 @@
 export BASE_URL=http://localhost:3123 && \
 export LOGIN_URL=https://login.salesforce.com && \
 
-# DEV
-export CLIENT_ID=3MVG9SemV5D80oBe9YTRxk_b.CeEzBMGBM4sQJUW8MBZjtESSLKCoLNSYn_d79r_qw.3NOExoVZ4gQxL6RCeZ && \
-export CLIENT_SECRET=5930902767596891690 && \
-export INSTANCE_URL=https://externalwavetest.my.salesforce.com && \
-export REFRESH_TOKEN=5Aep8614XGwzVzzTO54CmDqQVeOWipsp2bIlQ2Akykuk_VxlLVInLLjrhre7dZVRr98seMgFD0ofGR1L9MWXESd && \
+# DEV - Sales
+# export CLIENT_ID=3MVG9SemV5D80oBe9YTRxk_b.CeEzBMGBM4sQJUW8MBZjtESSLKCoLNSYn_d79r_qw.3NOExoVZ4gQxL6RCeZ && \
+# export CLIENT_SECRET=5930902767596891690 && \
+# export INSTANCE_URL=https://externalwavetest.my.salesforce.com && \
+# export REFRESH_TOKEN=5Aep8614XGwzVzzTO54CmDqQVeOWipsp2bIlQ2Akykuk_VxlLXxvi3yjcd_mt1AA_5yXEte74NZ4_wmSQWZtq_V && \
 
+# PRD - Service
+export CLIENT_ID=3MVG9LV8n9LkqoRNgfVXMjFpN_5nqW48rhzF_RRxS4PiY9zAF.RPlW4Iql9kksd_NEJKG1WlUJeVKkJksv0rz && \
+export CLIENT_SECRET=439351262686267344 && \
+export INSTANCE_URL=https://login.my.salesforce.com && \
+export REFRESH_TOKEN=5Aep861EvoqimEYZqEpnH1Jx4lxG97dp_TZHRtscUWeHzqZdKmaSGnzLQLHr9M2JWLvJ1o2tVWiROlkY2Tubd0f && \
 
 # # PRD
 # export CLIENT_ID=3MVG9CEn_O3jvv0xjKzbd8anvY_irzh5P.fA7JfAcNp7iTtDUIbDguPQJcTH7vyZZRmpdCfBlu3F5ppW_bthI && \
